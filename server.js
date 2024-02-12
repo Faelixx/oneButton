9b60299cf88af8ef758cd03c8e5048f78dc069ef
Faelixx <sasha.taylor7@gmail.com>
2024-02-11 22:14:58 -0700

    Feat: Press sagat to tiger shot

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+app.use(express.json());
+
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.render("mainpage");
 });
