fd5541804a6c87c39f122e9dd32c45a716342e6d
Sasha <sasha.taylor7@gmail.com>
2024-05-28 21:21:49 -0600

    Feat: all gifs animate

--- public/scripts/gifs.js
+++ public/scripts/gifs.js
@@ -2,29 +2,19 @@
 // Then replace the img with a gif on 
 // keydown
 
-const imgIds = {
-  //Sagat
-  tigerShot: document.getElementById('tiger-shot'),
-  tigerDp: document.getElementById('tiger-uppercut'),
-  tigerKDp: document.getElementById('tiger-knee'),
-  sagatTaunt: document.getElementById('sagat-taunt'),
-  //Ryu
-  ryuHadouken: document.getElementById('ryu-hadouken'),
-  ryuDp: document.getElementById('ryu-shoryuken'),
-  ryuTatsu: document.getElementById('ryu-tastu'),
-  ryuTaunt: document.getElementById('ryu-taunt'),
-  //Chun-Li
-  chunKiko: document.getElementById('chun-kiko'),
-  chunOh: document.getElementById('chun-oh'),
-  chunKick: document.getElementById('chun-kick'),
-  chunTaunt: document.getElementById('chun-taunt'),
-  //Dan
-  danYahoo: document.getElementById('dan-yahoo'),
-  danSuper: document.getElementById('dan-super'),
-  danVictory: document.getElementById('dan-victory'),
-  danFather: document.getElementById('dan-father'),
-  
-}
+const getElementsByIds = (ids) => {
+  return ids.reduce((acc, id) => {
+    acc[id] = document.getElementById(id);
+    return acc;
+  }, {});
+};
+
+const imgIds = getElementsByIds([
+  'tiger-shot', 'tiger-uppercut', 'tiger-knee', 'sagat-taunt', 
+  'ryu-hadouken', 'ryu-shoryuken', 'ryu-tatsu', 'ryu-taunt',
+  'chun-kiko', 'chun-oh', 'chun-kick', 'chun-taunt',
+  'dan-yahoo', 'dan-super', 'dan-victory', 'dan-father'
+]);
 
 const imgs = {
   sagat: {
@@ -62,6 +52,28 @@ const replaceImg = (img, newSrc) => {
   console.log("Image changed!");
 }
 
+const createReplaceActions = (imgId, imgSrc, stillImgSrc, duration) => {
+  return {
+    replaceDown() {
+      if (!this.isPlaying) {
+        this.isPlaying = true;
+        replaceImg(imgId, imgSrc);
+      }
+    },
+
+    replaceUp() {
+      if (typeof this.timeoutID === "number") {
+        this.cancel();
+      }
+
+          this.timeoutID = setTimeout(() => {
+            replaceImg(imgId, stillImgSrc);
+            this.isPlaying = false;
+          }, duration);
+    }
+  }
+}
+
 const replaceImgAction = {
   isPlaying: false,
 
@@ -69,67 +81,133 @@ const replaceImgAction = {
     clearTimeout(this.timeoutID);
     this.isPlaying = false;
   },
+};
 
-  tigerShotReplaceDown() {
-    if (!this.isPlaying){
-      this.isPlaying = true;
-      replaceImg(imgIds.tigerShot, imgs.sagat.tigerShot);
-    }
-  },
-
-  tigerShotReplaceUp() {
-    if (typeof this.timeoutID === "number") {
-      this.cancel();
-    }
-
-    this.timeoutID = setTimeout(() => {
-      replaceImg(imgIds.tigerShot, imgs.sagat.still);
-      this.isPlaying = false;
-    }, 1000)
-
-  },
-
-  //
-
-  tigerDpReplaceDown() {
-    if (!this.isPlaying){
-      this.isPlaying = true;
-      replaceImg(imgIds.tigerDp, imgs.sagat.tigerDp);
-    }
-  },
-
-  tigerDpReplaceUp() {
-    if (typeof this.timeoutID === "number") {
-      this.cancel();
-    }
-
-    this.timeoutID = setTimeout(() => {
-      replaceImg(imgIds.tigerDp, imgs.sagat.still);
-      this.isPlaying = false;
-    }, 1300)
-
-  },
-
-  //
+const sagatActions = {
+  tigerShot: createReplaceActions(imgIds['tiger-shot'], imgs.sagat.tigerShot, imgs.sagat.still, 1000),
+  tigerDp: createReplaceActions(imgIds['tiger-uppercut'], imgs.sagat.tigerDp, imgs.sagat.still, 1300),
+  tigerKDp: createReplaceActions(imgIds['tiger-knee'], imgs.sagat.tigerKDp, imgs.sagat.still, 500),
+  sagatTaunt: createReplaceActions(imgIds['sagat-taunt'], imgs.sagat.sagatTaunt, imgs.sagat.still, 900),
+};
+const ryuActions = {
+  ryuHadouken: createReplaceActions(imgIds['ryu-hadouken'], imgs.ryu.ryuHadouken, imgs.ryu.still, 1300),
+  ryuDp: createReplaceActions(imgIds['ryu-shoryuken'], imgs.ryu.ryuDp, imgs.ryu.still, 1300),
+  ryuTatsu: createReplaceActions(imgIds['ryu-tatsu'], imgs.ryu.ryuTatsu, imgs.ryu.still, 1300),
+  ryuTaunt: createReplaceActions(imgIds['ryu-taunt'], imgs.ryu.ryuTaunt, imgs.ryu.still, 1300),
 
 };
+const chunActions = {
+  chunKiko: createReplaceActions(imgIds['chun-kiko'], imgs.chun.chunKiko, imgs.chun.still, 1300),
+  chunOh: createReplaceActions(imgIds['chun-oh'], imgs.chun.chunOh, imgs.chun.still, 1300),
+  chunKick: createReplaceActions(imgIds['chun-kick'], imgs.chun.chunKick, imgs.chun.still, 1300),
+  chunTaunt: createReplaceActions(imgIds['chun-taunt'], imgs.chun.chunTaunt, imgs.chun.still, 1300),
+};
+const danActions = {
+  danYahoo: createReplaceActions(imgIds['dan-yahoo'], imgs.dan.danYahoo, imgs.dan.still, 1300),
+  danSuper: createReplaceActions(imgIds['dan-super'], imgs.dan.danSuper, imgs.dan.still, 2300),
+  danVictory: createReplaceActions(imgIds['dan-victory'], imgs.dan.danVictory, imgs.dan.still, 1300),
+  danFather: createReplaceActions(imgIds['dan-father'], imgs.dan.danFather, imgs.dan.still, 800),
+};
+
+Object.assign(replaceImgAction, sagatActions);
+for (const key in replaceImgAction) {
+  if (typeof replaceImgAction[key] === 'function') {
+    replaceImgAction[key] = replaceImgAction[key].bind(replaceImgAction);
+  }
+}
+
+Object.assign(replaceImgAction, ryuActions);
+for (const key in replaceImgAction) {
+  if (typeof replaceImgAction[key] === 'function') {
+    replaceImgAction[key] = replaceImgAction[key].bind(replaceImgAction);
+  }
+}
+
+Object.assign(replaceImgAction, chunActions);
+for (const key in replaceImgAction) {
+  if (typeof replaceImgAction[key] === 'function') {
+    replaceImgAction[key] = replaceImgAction[key].bind(replaceImgAction);
+  }
+}
+
+Object.assign(replaceImgAction, danActions);
+for (const key in replaceImgAction) {
+  if (typeof replaceImgAction[key] === 'function') {
+    replaceImgAction[key] = replaceImgAction[key].bind(replaceImgAction);
+  }
+}
 
 
 const imgAction = {
+  //
   1: { 
-    keydown: replaceImgAction.tigerShotReplaceDown,
-    keyup: replaceImgAction.tigerShotReplaceUp
+    keydown: replaceImgAction.tigerShot.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.tigerShot.replaceUp.bind(replaceImgAction)
     },
   2: { 
-    keydown: replaceImgAction.tigerDpReplaceDown,
-    keyup: replaceImgAction.tigerDpReplaceUp
-    }
+    keydown: replaceImgAction.tigerDp.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.tigerDp.replaceUp.bind(replaceImgAction)
+    },
+  3: { 
+    keydown: replaceImgAction.tigerKDp.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.tigerKDp.replaceUp.bind(replaceImgAction)
+    },
+  4: { 
+    keydown: replaceImgAction.sagatTaunt.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.sagatTaunt.replaceUp.bind(replaceImgAction)
+    },
+  //
+  'q': { 
+    keydown: replaceImgAction.ryuHadouken.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.ryuHadouken.replaceUp.bind(replaceImgAction)
+    },
+  'w': { 
+    keydown: replaceImgAction.ryuDp.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.ryuDp.replaceUp.bind(replaceImgAction)
+    },
+  'e': { 
+    keydown: replaceImgAction.ryuTatsu.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.ryuTatsu.replaceUp.bind(replaceImgAction)
+    },
+  'r': { 
+    keydown: replaceImgAction.ryuTaunt.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.ryuTaunt.replaceUp.bind(replaceImgAction)
+    },
+  //
+  'a': { 
+    keydown: replaceImgAction.chunKiko.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.chunKiko.replaceUp.bind(replaceImgAction)
+    },
+  's': { 
+    keydown: replaceImgAction.chunOh.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.chunOh.replaceUp.bind(replaceImgAction)
+    },
+  'd': { 
+    keydown: replaceImgAction.chunKick.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.chunKick.replaceUp.bind(replaceImgAction)
+    },
+  'f': { 
+    keydown: replaceImgAction.chunTaunt.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.chunTaunt.replaceUp.bind(replaceImgAction)
+    },
+  //
+  'z': { 
+    keydown: replaceImgAction.danYahoo.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.danYahoo.replaceUp.bind(replaceImgAction)
+    },
+  'x': { 
+    keydown: replaceImgAction.danSuper.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.danSuper.replaceUp.bind(replaceImgAction)
+    },
+  'c': { 
+    keydown: replaceImgAction.danVictory.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.danVictory.replaceUp.bind(replaceImgAction)
+    },
+  'v': { 
+    keydown: replaceImgAction.danFather.replaceDown.bind(replaceImgAction),
+    keyup: replaceImgAction.danFather.replaceUp.bind(replaceImgAction)
+    },
+  //
 };
 
-replaceImgAction.tigerShotReplaceDown = replaceImgAction.tigerShotReplaceDown.bind(replaceImgAction);
-replaceImgAction.tigerShotReplaceUp = replaceImgAction.tigerShotReplaceUp.bind(replaceImgAction);
-replaceImgAction.cancel = replaceImgAction.cancel.bind(replaceImgAction);
-
-replaceImgAction.tigerDpReplaceDown = replaceImgAction.tigerDpReplaceDown.bind(replaceImgAction);
-replaceImgAction.tigerDpReplaceUp = replaceImgAction.tigerDpReplaceUp.bind(replaceImgAction);
 replaceImgAction.cancel = replaceImgAction.cancel.bind(replaceImgAction);
